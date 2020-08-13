fbe7e643684786262eac2ee98ad622df24fb86eb
pounlaura <61973416+pounlaura@users.noreply.github.com>
2020-08-13 11:07:36 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -91,7 +91,7 @@ var allDataUrl = 'https://pounlaura.github.io/BERDO-data-tool/BERDO_2019_All.geo
           .enter().append('g')
           .attr('class', 'bar')
           .attr('transform', function(d){
-            return "translate(" + x(d.x0) + "," + y(d.length) + ")";
+            return "translate(" 50 + x(d.x0) + "," + y(d.length) + ")";
           });
 
           bar.append('rect')
@@ -102,7 +102,7 @@ var allDataUrl = 'https://pounlaura.github.io/BERDO-data-tool/BERDO_2019_All.geo
 
           graphSVG.append("g")
           .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
+          .attr("transform", "translate(50," + height + ")")
           .call(xAxis);
 
           graphSVG.append('g')
